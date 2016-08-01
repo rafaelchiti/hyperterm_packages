26b84459e2d0e721484cfd4882a63ed0387f59c7
Rafael Chiti <rafaelchiti@gmail.com>
2016-08-02 00:23:19 +0100

    Adding some styles to the package component

--- webapp/views/hyperterm_packages_list/index.jsx
+++ webapp/views/hyperterm_packages_list/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import HypertermPackage from './hyperterm_package';
+import styles from './styles';
+
 
 export default class HypertermPackagesList extends Component {
   static propTypes = {
@@ -8,7 +10,9 @@ export default class HypertermPackagesList extends Component {
 
   render() {
     const children = this.props.items.map((item, i) => (
-      <HypertermPackage key={i} item={item} />
+      <div key={i} className={styles.listItem}>
+        <HypertermPackage item={item} />
+      </div>
     ));
 
     return (
